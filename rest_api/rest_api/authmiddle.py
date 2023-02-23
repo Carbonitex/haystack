e4d28d76216690e83fc3c80f8e196405c4127ee5
Matt Petty <19671350+Carbonitex@users.noreply.github.com>
2023-02-22 19:21:16 -0700

    add logger

--- rest_api/rest_api/authmiddle.py
+++ rest_api/rest_api/authmiddle.py
@@ -1,16 +1,19 @@
 from typing import Any, Callable, Coroutine
 from fastapi import Response, Request
 from rest_api.config import TOKEN_CRED
+import logging
 
+logging.getLogger("haystack").setLevel("INFO")
+logger = logging.getLogger("haystack")
 
 async def auth_middleware(request: Request, call_next: Callable, some_attribute: Any) -> Response:
     request.state.attr = some_attribute  # Do what you need with your attribute
     content_type = request.headers.get('Content-Type')
-    print(content_type)
-    print("test2")
+    logger.info(content_type)
+    logger.info("test2")
     token_auth = request.headers.get('Token-Auth')
-    print(token_auth)
-    print("test")
-    print(TOKEN_CRED)
-    print("test3")
+    logger.info(token_auth)
+    logger.info("test")
+    logger.info(TOKEN_CRED)
+    logger.info("test3")
     return await call_next(request)
